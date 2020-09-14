5ae291f5e9be17683d9cddb59a8b5c3165685f1a
Evan Quinlan <EQuinlan@Transparent.com>
2020-09-14 17:00:44 -0400

    Remove bucket_test_b from tests list Add app files to Files section of README Deploy application

--- mlna-web/src/app/tests.service.ts
+++ mlna-web/src/app/tests.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TestsService {
-  tests: string[] = ['bucket_test_a', 'bucket_test_b']
+  tests: string[] = ['bucket_test_a']
 
   constructor() { }
 
