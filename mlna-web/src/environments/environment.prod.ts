7fb0ce4f6e308fbfdd19d6e2e3c776d32772a3a2
Evan Quinlan <EQuinlan@Transparent.com>
2020-09-16 17:12:44 -0400

    Add environments files.

---
+++ mlna-web/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCddkuLSrw3q4zGOvkYXjbpwgqrygluZ2w",
+    authDomain: "mlna-udacity.firebaseapp.com",
+    databaseURL: "https://mlna-udacity.firebaseio.com",
+    projectId: "mlna-udacity",
+    storageBucket: "mlna-udacity.appspot.com",
+    messagingSenderId: "694366459007",
+    appId: "1:694366459007:web:5a8c2c8eed27dc5cfc1b4a"
+  }
+};
---
+++ mlna-web/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCddkuLSrw3q4zGOvkYXjbpwgqrygluZ2w",
+    authDomain: "mlna-udacity.firebaseapp.com",
+    databaseURL: "https://mlna-udacity.firebaseio.com",
+    projectId: "mlna-udacity",
+    storageBucket: "mlna-udacity.appspot.com",
+    messagingSenderId: "694366459007",
+    appId: "1:694366459007:web:5a8c2c8eed27dc5cfc1b4a"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
